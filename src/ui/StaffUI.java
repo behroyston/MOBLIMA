fd6e4a68599e0dfca15f68a3e46fc9e9705b1833
Amos Chua <TCHUA006@e.ntu.edu.sg>
2017-11-14 16:40:34 +0800

    Update StaffUI.java
    Added login validation and do while

--- src/ui/StaffUI.java
+++ src/ui/StaffUI.java
@@ -17,68 +17,71 @@ public class StaffUI {
 	
 	//display
 	public void display() {
-		System.out.println("-----------------STAFF CONSOLE-----------------");
-		System.out.println("What would you like to do?\n");
-		System.out.println("1. Create Movie Listing\n");
-		System.out.println("2. Update Movie Listing\n");
-		System.out.println("3. Remove Movie Listing\n");
-		System.out.println("4. Create Cinema Showtimes\n");
-		System.out.println("5. Update Cinema Showtimes\n");
-		System.out.println("6. Remove Cinema Showtimes\n");
-		System.out.println("7. Configure System Settings\n");
-		System.out.println("0. Exit\n");
-		int choice1 = sc.nextInt();
-		
-		switch (choice1) {
-		case '1':
-			createMovieListing();
-			break;
-		case '2':
-			updateMovieListing();
-			break;
-		case '3':
-			removeMovieListing();
-			break;
-		case '4':
-			createCinemaShowtimes();
-			break;
-		case '5':
-			updateCinemaShowtimes();
-			break;
-		case '6':
-			removeCinemaShowtimes();
-			break;
-		case '7':
-			configSystemSettings();
-			break;
-		case '0':
-			//exit???
-			break;
+		// Validation first
+		System.out.println("-------------------- Welcome to MOBLIMA! --------------------");
+		String emailAddress = loginValidation();
+		if (emailAddress == null){
+			return;
 		}
+		
+		int choice1;
+		
+		do {
+			System.out.println("-----------------STAFF CONSOLE-----------------");
+			System.out.println("What would you like to do?\n");
+			System.out.println("1. Create Movie Listing\n");
+			System.out.println("2. Update Movie Listing\n");
+			System.out.println("3. Remove Movie Listing\n");
+			System.out.println("4. Create Cinema Showtimes\n");
+			System.out.println("5. Update Cinema Showtimes\n");
+			System.out.println("6. Remove Cinema Showtimes\n");
+			System.out.println("7. Configure System Settings\n");
+			System.out.println("0. Exit\n");
+			choice1 = sc.nextInt();
+		
+			switch (choice1) {
+			case '1':
+				createMovieListing();
+				break;
+			case '2':
+				updateMovieListing();
+				break;
+			case '3':
+				removeMovieListing();
+				break;
+			case '4':
+				createCinemaShowtimes();
+				break;
+			case '5':
+				updateCinemaShowtimes();
+				break;
+			case '6':
+				removeCinemaShowtimes();
+				break;
+			case '7':
+				configSystemSettings();
+				break;
+			}
+		} while (choice1 != 0);
 	}
 	
 	//login
-	private void loginValidation() {
-		
-		String password = null;
-		String name = null;
-		int mobileNumber = 0;
-		String email = null;
-		int staffID = 0;
-		Staff staff  = new Staff(password, name, mobileNumber, email, staffID);
-		
-		System.out.println("----------------------STAFF LOGIN------------------------");
-        if (staff.login()) {
-                System.out.println("Login Successful! ");
-                System.out.println("Redirecting to Staff Console... ");
-                display();
-        }
-        else {
-        	System.out.println("Login failed. Please try again.");
-        	loginValidation();
-        }
-		
+	private String loginValidation() {
+
+		String email, password;
+		while (true){
+			System.out.print("Please enter your email address: ");
+			email = sc.next();
+			System.out.print("Please enter your password: ");
+			password = sc.next();
+			if (!StaffController.getInstance().validateStaff(email, password))
+				System.out.println("Invalid Staff login details! Please re-enter...");
+			else
+				break;
+		}
+		return email;
 	}
+
 	
 	//methods to edit movie listing
 	private void createMovieListing() {
@@ -211,95 +214,98 @@ public class StaffUI {
 		System.out.println("0. Exit\n");
 		int choice2 = sc.nextInt();
 		
-		switch(choice2) {
-		case '1':
-			System.out.println("Current Base Price: " + SystemSettings.getInstance().getBase_price() );
-			System.out.println("Would you like to amend the Base Price? Enter '1' if YES; '0' if NO. ");
-			int choice2a = sc.nextInt();
-			if(choice2a == 1) {
-				System.out.println("New Base Price: ");
-				double newBase = sc.nextDouble();
-				SystemSettings.getInstance().setBase_price(newBase);
-			}
-			else if(choice2a != 0 && choice2a != 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+		
+		do {
+			switch(choice2) {
+			case '1':
+				System.out.println("Current Base Price: " + SystemSettings.getInstance().getBase_price() );
+				System.out.println("Would you like to amend the Base Price? Enter '1' if YES; '0' if NO. ");
+				int choice2a = sc.nextInt();
+				if(choice2a == 1) {
+					System.out.println("New Base Price: ");
+					double newBase = sc.nextDouble();
+					SystemSettings.getInstance().setBase_price(newBase);
+				}
+				else if(choice2a != 0 && choice2a != 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 				
-		case '2':
-			System.out.println("Current 3D Movie Surcharge: " + SystemSettings.getInstance().getThreeDExtra() );
-			System.out.println("Would you like to amend 3D Movie Surcharge? Enter '1' if YES; '0' if NO. ");
-			int choice2b = sc.nextInt();
-			if(choice2b == 1) {
-				System.out.println("New 3D Surcharge: ");
-				double newThreeD = sc.nextDouble();
-				SystemSettings.getInstance().setThreeDExtra(newThreeD);;
-			}
-			else if(choice2b != 0 && choice2b != 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+			case '2':
+				System.out.println("Current 3D Movie Surcharge: " + SystemSettings.getInstance().getThreeDExtra() );
+				System.out.println("Would you like to amend 3D Movie Surcharge? Enter '1' if YES; '0' if NO. ");
+				int choice2b = sc.nextInt();
+				if(choice2b == 1) {
+					System.out.println("New 3D Surcharge: ");
+					double newThreeD = sc.nextDouble();
+					SystemSettings.getInstance().setThreeDExtra(newThreeD);;
+				}
+				else if(choice2b != 0 && choice2b != 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 			
-		case '3':
-			System.out.println("Current Platinum Suite Surcharge: " + SystemSettings.getInstance().getPlatinumExtra() );
-			System.out.println("Would you like to amend the Platinum Suite Surcharge? Enter '1' if YES; '0' if NO. ");
-			int choice2c = sc.nextInt();
-			if(choice2c == 1) {
-				System.out.println("New Platinum Suite Surcharge: ");
-				double newPlat = sc.nextDouble();
-				SystemSettings.getInstance().setPlatinumExtra(newPlat);;
-			}
-			else if(choice2c != 0 && choice2c!= 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+			case '3':
+				System.out.println("Current Platinum Suite Surcharge: " + SystemSettings.getInstance().getPlatinumExtra() );
+				System.out.println("Would you like to amend the Platinum Suite Surcharge? Enter '1' if YES; '0' if NO. ");
+				int choice2c = sc.nextInt();
+				if(choice2c == 1) {
+					System.out.println("New Platinum Suite Surcharge: ");
+					double newPlat = sc.nextDouble();
+					SystemSettings.getInstance().setPlatinumExtra(newPlat);;
+				}
+				else if(choice2c != 0 && choice2c!= 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 			
-		case '4':
-			System.out.println("Current Senior Citizen Discount: " + SystemSettings.getInstance().getSeniorDiscount() );
-			System.out.println("Would you like to amend the Senior Citizen Discount? Enter '1' if YES; '0' if NO. ");
-			int choice2d = sc.nextInt();
-			if(choice2d == 1) {
-				System.out.println("New Senior Citien Discount: ");
-				double newSnr = sc.nextDouble();
-				SystemSettings.getInstance().setSeniorDiscount(newSnr);;
-			}
-			else if(choice2d != 0 && choice2d != 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+			case '4':
+				System.out.println("Current Senior Citizen Discount: " + SystemSettings.getInstance().getSeniorDiscount() );
+				System.out.println("Would you like to amend the Senior Citizen Discount? Enter '1' if YES; '0' if NO. ");
+				int choice2d = sc.nextInt();
+				if(choice2d == 1) {
+					System.out.println("New Senior Citien Discount: ");
+					double newSnr = sc.nextDouble();
+					SystemSettings.getInstance().setSeniorDiscount(newSnr);;
+				}
+				else if(choice2d != 0 && choice2d != 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 			
-		case '5':
-			System.out.println("Current Child Discount: " + SystemSettings.getInstance().getChildDiscount() );
-			System.out.println("Would you like to amend the Child Discount? Enter '1' if YES; '0' if NO. ");
-			int choice2e = sc.nextInt();
-			if(choice2e == 1) {
-				System.out.println("New Child Discount: ");
-				double newChild = sc.nextDouble();
-				SystemSettings.getInstance().setChildDiscount(newChild);;
-			}
-			else if(choice2e != 0 && choice2e != 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+			case '5':
+				System.out.println("Current Child Discount: " + SystemSettings.getInstance().getChildDiscount() );
+				System.out.println("Would you like to amend the Child Discount? Enter '1' if YES; '0' if NO. ");
+				int choice2e = sc.nextInt();
+				if(choice2e == 1) {
+					System.out.println("New Child Discount: ");
+					double newChild = sc.nextDouble();
+					SystemSettings.getInstance().setChildDiscount(newChild);;
+				}
+				else if(choice2e != 0 && choice2e != 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 			
-		case '6':
-			System.out.println("Current Weekend/Holiday Surcharge: " + SystemSettings.getInstance().getWeekend_HolidayExtra() );
-			System.out.println("Would you like to amend the Weekend/Holiday Surcharge? Enter '1' if YES; '0' if NO. ");
-			int choice2f = sc.nextInt();
-			if(choice2f == 1) {
-				System.out.println("New Weekend/Holiday Surcharge: ");
-				double newWeek = sc.nextDouble();
-				SystemSettings.getInstance().setWeekend_HolidayExtra(newWeek);;
-			}
-			else if(choice2f != 0 && choice2f != 1) {
-				System.out.println("INVALID OPTION");
-			}
-			else break;
+			case '6':
+				System.out.println("Current Weekend/Holiday Surcharge: " + SystemSettings.getInstance().getWeekend_HolidayExtra() );
+				System.out.println("Would you like to amend the Weekend/Holiday Surcharge? Enter '1' if YES; '0' if NO. ");
+				int choice2f = sc.nextInt();
+				if(choice2f == 1) {
+					System.out.println("New Weekend/Holiday Surcharge: ");
+					double newWeek = sc.nextDouble();
+					SystemSettings.getInstance().setWeekend_HolidayExtra(newWeek);;
+				}
+				else if(choice2f != 0 && choice2f != 1) {
+					System.out.println("INVALID OPTION");
+				}
+				else break;
 			
-		case '0':
-			//EXIT?!??!
-			break;
-		}
+			case '0':
+				//EXIT?!??!
+				break;
+			}
+		} while (choice2 != 0);
 	}
 	
 	//static 'instance' method
